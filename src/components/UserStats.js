36597a3df19ba1a563a8fef0a1aaf7426aed3b6e
Chris Paredes <chrisparedes206@gmail.com>
2018-08-10 15:03:43 -0700

    Feat: Application Functionality
    Update UserStats styles

--- src/components/UserStats.js
+++ src/components/UserStats.js
@@ -1,17 +1,15 @@
 import React from 'react'
 
 export default function UserStats () {
-  const divStyle = { maxWidth: '618px' }
-  const rightColumnStyle = { backgroundColor: 'blue' }
   return (
-    <div class='card text-left m-4' style={divStyle}>
-      <div class='card-body p-0'>
+    <div class='card text-left mb-3'>
       <div class='card-header'>Joe Schmoe</div>
-        <div class='row pl-5 pt-2 pb-2 pr-5'>
-          <div class='column mr-4'>
+      <div class='card-body p-0'>
+        <div class='row ml-0 mr-0'>
+          <div class='column border-right p-3'>
             <img src='avatars/1.jpg' class='img-fluid rounded-circle'/>
           </div>
-          <div class='column ml-10 pl-10'>
+          <div class='column ml-3 p-3'>
             <p class='card-text font-weight-bold'>Questions Asked: 1</p>
             <p class='card-text font-weight-bold'>Questions Answered: 1</p>
           </div>
