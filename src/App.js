d65087ac3cbeda56ea0df4ecd56d075f2cc036a8
Chris Paredes <chrisparedes206@gmail.com>
2018-08-12 12:57:40 -0700

    Feat: Application Functionality
    Create first reducers

--- src/App.js
+++ src/App.js
@@ -1,79 +1,13 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
-import Nav from './components/Nav'
-import LoginForm from './components/LoginForm'
-import LeaderBoard from './components/LeaderBoard'
-import QuestionList from './components/QuestionList'
-import AddQuestion from './components/AddQuestion'
-import { BrowserRouter } from 'react-router-dom'
-
-
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        { /* TODO: This is just a stub for design purposes. LoginForm should
-                   display as a guard on any page when user is not logged in.
-                   The home page should display at the '/' path */ }
-        <Route exact path='/' render={() => (
-          <div class='text-center'>
-            <Nav />
-            <div class='container'>
-              <LoginForm />
-            </div>
-          </div>
-        )}/>
-
-        <Route exact path='/home' render={() => (
-          <div>
-            <Nav />
-            <QuestionList viewType='unanswered' />
-          </div>
-        )}/>
-
-        { /* TODO: Delete this when state is added */ }
-        <Route exact path='/answered-questions' render={() => (
-          <div>
-            <Nav />
-            <QuestionList viewType='answered' />
-          </div>
-        )}/>
-
-        <Route exact path='/leaderboard' render={() => (
-          <div>
-            <Nav />
-            <LeaderBoard />
-          </div>
-        )}/>
-
-        { /* TODO: this should be /questions/:question_id when state is added */ }
-        <Route exact path='/unanswered-question' render={() => (
-          <div class='text-center'>
-            <Nav />
-            <QuestionList viewType='forSubmission' />
-          </div>
-        )}/>
-
-        { /* TODO: this should be /questions/:question_id when state is added */ }
-        <Route exact path='/answered-question' render={() => (
-          <div class='text-center'>
-            <Nav />
-            <QuestionList viewType='withStats' />
-          </div>
-        )}/>
-
-        <Route exact path='/add' render={() => (
-          <div class='text-center'>
-          <Nav />
-          <AddQuestion />
-          </div>
-        )}/>
+      <div>
+        Starter Code
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+
+const store = createStore(reducer)
 
 ReactDOM.render(
-  <BrowserRouter><App /></BrowserRouter>,
-  document.getElementById('root')
-);
-registerServiceWorker();
+  <Provider store={store}>
+    <App />
+  </Provider>,
+document.getElementById('root')
+)
