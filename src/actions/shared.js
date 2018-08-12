b551cfb7d24f64762cf28c98a49c497cae4d7f7d
Chris Paredes <chrisparedes206@gmail.com>
2018-08-12 13:16:43 -0700

    Feat: Application Functionality
    Set up initial data

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,6 @@
 import { getInitialData } from '../utils/api'
 import { receiveUsers } from '../actions/users'
-import { receiveTweets } from '../actions/questions'
+import { receiveQuestions } from '../actions/questions'
 import { setAuthedUser } from '../actions/authedUser'
 
 const AUTHED_ID = 'tylermcginnis'
@@ -10,7 +10,7 @@ export function handleInitialData () {
     return getInitialData()
       .then(({ users, questions }) => {
         dispatch(receiveUsers(users))
-        dispatch(receiveQuestions(tweets))
+        dispatch(receiveQuestions(questions))
         dispatch(setAuthedUser(AUTHED_ID))
       })
   }
